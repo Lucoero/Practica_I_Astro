50eb3ae4b75d29168706c3adda9efbbbe2b1ad26
Sigzt <56723625+Sigzt@users.noreply.github.com>
2026-03-10 20:20:01 +0100

    Encontrar picos
    callate copilot

--- Codigos/parametros.py
+++ Codigos/parametros.py
@@ -4,17 +4,27 @@ parametros:
     diversos parametros como la Temperatura y demas
 """
 
+
+import re
 import numpy as np
+from astropy.io import fits
+import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy.visualization import quantity_support
+from pathlib import Path
+from scipy.signal import convolve
+from scipy.signal import find_peaks, peak_prominences, peak_widths
+
 
 #%% Temperatura
 
-def get_Temp(lamb,flux): #Saca temperatura con Ley Wien
+def get_Temp(lamb, flux): #Saca temperatura con Ley Wien
     """
     Ley de Wien: 
         lamb_max*T= b
     """
 
-    b= 2.8976e-7 #Angstrom
+    b= 2.8976e7 #Angstrom
 
     index= np.where(np.max(flux))
     """
@@ -23,8 +33,29 @@ def get_Temp(lamb,flux): #Saca temperatura con Ley Wien
     de onda
     """
 
-    lamb_max= lamb[index]
+    lamb_max = lamb[index]
 
     T= b/lamb_max
 
     return T
+
+def picos (wave, flux, lineas, dist=100):
+    '''
+    La salida de la función es del tipo [picos,[prom,ancho]]; siendo cada elemento un array. La función siempre encuentra pico
+    '''
+    peaks, props = [], []   #se inicilizan las listas vacías (sé que el append es lento de cojones pero macho que son 4 cosas
+    for line in lineas:     
+        center = np.searchsorted(wave, lineas[line], side='right') #pilla el i para que el i-ésimo componente de los datos sea el más cercano a la linea dada
+        a = center - dist    
+        b = center + dist
+        rango = -1*flux[a:b] #se delimita el rango de búsqueda de picos 
+        ind, prop = find_peaks(rango, distance=2*dist) #el find peaks busca el pico más `significativo` que hay en el rango 
+        if ind == []:    #por si no hay picos
+            break
+        ind += a    #se recentra el índice para los datos de longitud de onda dados
+        propiedades = [peak_prominences(rango, ind)[0], peak_widths(rango, ind)[0]] #información adicional de los picos
+        peaks.append(wave[ind])
+        props.append(propiedades) #se añaden los datos a la lista. se pude hacer sin el apend pero es más coñazo porque la entrada es un diccionario
+    return peaks, props
+    
+      
