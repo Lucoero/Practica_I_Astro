e66590fb151ce80fa00972d3e62614ed444436bc
Lucoero <lucasnhp227@gmail.com>
2026-03-10 19:18:39 +0100

    compare

--- Codigos/Main.py
+++ Codigos/Main.py
@@ -9,15 +9,26 @@ import Show_Spectra as SSp
 
 plt.close("all")
 #%% Variables de llamada
-Prob_Name = "estrella4.dat"
+# Estrellas problema
+S1 = "estrella1.dat"
+S2 = "estrella2.dat"
+S3 = "estrella3.dat"
+S4 = "estrella4.dat"
+
+# Espectros del catalogo MILES
 Miles_Name = "s0010.fits"
+
+
+# Diccionario de lineas que vamos a marcar
 #%% Obtencion de datos
 
-PLamb, PFlux = LD.Load_Dat(Prob_Name)
+Lamb1, Flux1 = LD.Load_Dat(S1)
 
+Lamb2,Flux2 = LD.Load_Dat(S2)
 #MLamb,MFlux = LD.Load_Miles(Miles_Name)
 #%% Correcciones
 #%% Ploteado
 
-SSp.Blank_Spectra(PLamb,PFlux)
+SSp.Compare_Spectra(Lamb1,Flux1,Lamb2,Flux2)
+
 #SSp.Blank_Spectra(MLamb, MFlux)
--- Codigos/Show_Spectra.py
+++ Codigos/Show_Spectra.py
@@ -8,7 +8,10 @@ Dado los arrays de datos, ploteamos los espectros con distintas propiedades
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#%% Variables globales de plots
+yscale = 1.01 # Escala para escoger el tamanno de los plots
+linewidth = 1 # Grosor de las lineas de plot
+#%% Funciones
 def Blank_Spectra(lamb,flux, title = "Espectro", onlyObject = False):
     """
     Blank_Spectra:
@@ -18,7 +21,7 @@ def Blank_Spectra(lamb,flux, title = "Espectro", onlyObject = False):
     """
     fig, ax = plt.subplots(figsize = (15,6))
     fig.suptitle(title)
-    ax.set_xlabel(r"$\lambda \ (A^{\circ})$")
+    ax.set_xlabel(r"$\lambda \ (\mathring{A})$")
     ax.set_ylabel("Flujo (unidades arbitrarias)")
     ax.plot(lamb,flux, linewidth = 1)
     if onlyObject:
@@ -34,15 +37,42 @@ def Lined_Spectra(lamb,flux,lines, title = "Lined_Spectra"):
     """
     # Ploteamos espectro
     fig,ax = Blank_Spectra(lamb, flux, title = title, onlyObject = True)
-    minLine = np.min(flux)*10
-    maxLine = np.max(flux)*10
+    minLine = np.min(flux)*yscale
+    maxLine = np.max(flux)*yscale
     # Ploteamos las lineas
     for name in lines:
         ax.vlines(lines[name], minLine,maxLine )
     fig.show()
     return
 
-def Compare_Spectra():
+def Compare_Spectra(lamb1,flux1,lamb2,flux2,lines = {}, title = "Comparison between Spectras"):
     """
-    Co
+    Compare_Spectra:
+        Ploteamos uno al lado del otro dos espectros para compararlos. Si se dan lineas
+        como parametro opcional, las coloca tambien en ambos
     """
+    fig,ax = plt.subplots(2,1, figsize = (60,6),sharex=True, sharey=False)
+    ax[1].set_xlabel(r"$\lambda\ (\mathring{A})$")
+    
+    ax[1].set_ylabel("Flujo (uds arbitrarias)")
+    ax[0].set_ylabel("Flujo (uds arbitrarias)")
+    
+    ax[0].set_title("Spectra 1")
+    ax[1].set_title("Spectra 2")
+    
+    #Ploteamos los espectros
+    ax[0].plot(lamb1,flux1,linewidth = 1)
+    ax[1].plot(lamb2,flux2,linewidth = 1)
+    
+    # Ploteamos las lineas
+    minLine = np.min((flux1,flux2))*yscale
+    maxLine = np.max((flux1,flux2))*yscale
+    
+    ax[0].set_ylim(minLine,maxLine)
+    ax[1].set_ylim(minLine,maxLine)
+    for name in lines:
+        ax[0].vlines(lines[name], minLine,maxLine, color = "red")
+        ax[1].vlines(lines[name], minLine,maxLine, color = "red")
+    fig.show()
+    return
+    
