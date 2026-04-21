b244f742754cb6d2ec2b82068b10a3737097793f
Sigzt <56723625+Sigzt@users.noreply.github.com>
2026-04-21 18:44:52 +0200

    pusheo de los cojonoes
    puta puta puta puta puta puta puta puta cojones ostia joder mierda puta puta puta puta puta puta ostia cojones

--- Codigos/normalizar_experimental.py
+++ Codigos/normalizar_experimental.py
@@ -1,13 +1,70 @@
-
 import numpy as np
+from scipy.signal import find_peaks, peak_prominences, peak_widths, find_peaks_cwt, savgol_filter
 
 import normalizar
 from parametros import *
 import Herramientas
 
+def picos2(wave, flux, lineas, d):
+    pki = []
+    for li in lineas:
+        center = np.searchsorted(wave,li,side='right')
+        a = center - d
+        b = center + d
+        rango = -1*flux[a:b]
+        ind, prop = find_peaks(rango, distance = 2*d)
+        if len(ind)==0:
+            print("error")
+        else:
+            ind +=a
+            pki.append(*ind)
+    return pki
 
 
-def puta(f,p,d):
+def continuo(fr,pr=0.1,d=5,sg=False):
+    if sg:
+        f = savgol_filter(fr,5,2)
+    else:
+        f = fr
+    pk1, _ = find_peaks(-1*f,prominence=pr,distance=d)
+    w1 = peak_widths(-1*f,pk1,rel_height=0.5)
+    pk2, _ = find_peaks(f,prominence=pr,distance=d)
+    w2 = peak_widths(f,pk2,rel_height=0.5)
+    pks = np.append(pk1,pk2)
+    ws = np.append(w1,w2)
+    aux = np.zeros_like(fr)
+    for i in range(len(pks)):
+        a = int(pks[i] - ws[i])
+        b = int(pks[i] + ws[i])
+        lsup = f[b]
+        linf = f[a]
+        pe = (lsup - linf)/(2*ws[i])
+        for t in range(int(a),int(b+1)):
+            aux[t] = linf + pe*(t-a)
+    for j in range(len(f)):
+        if aux[j] == 0:
+            aux[j] = f[j]
+    return aux
+
+def lucas(l,fr,rang,pr=0.1,d=10):
+
+    a = continuo(fr,sg=True)
+
+    norm = fr/a
+
+    plt.plot(l,norm)
+    plt.show()
+
+
+def npcont(l,f,pr=0.1,w=5,d=3):
+    pk1, _ = find_peaks(-1*f,prominence=pr,width=w,distance=d)
+    pk2, _ = find_peaks(f,prominence=pr,width=w,distance=d)
+    pks = np.append(pk1,pk2)
+    plt.plot(l,f,l[pks],f[pks],'*')
+    plt.show()
+    return pks
+
+def diflin(f,p,d): #aquí p es el índice del pico
     aux = np.zeros(len(f))
     for i in range(len(p)):
         a = p[i] - d
@@ -15,7 +72,7 @@ def puta(f,p,d):
         lsup = f[b]
         linf = f[a]
         pe = (lsup - linf)/(2*d)
-        for t in range(a,b+1):
+        for t in range(int(a),int(b+1)):
             aux[t] = linf + pe*(t-a)
     for j in range(len(f)):
         if aux[j] == 0:
@@ -25,8 +82,8 @@ def puta(f,p,d):
 
 
 
-def hola(wave,flux,lines, d = 15):
-    pks = picos(wave,flux,lineas,15)
-    rr = puta(flux,pks,d)
+def hola(wave,flux,lineas, d = 15):
+    pks = picos2(wave,flux,lineas,d)
+    rr = diflin(flux,pks,d)
     norm = flux/rr
     return norm
